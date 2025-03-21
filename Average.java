16e8e13bd9f18ca5e97494b1c363d6a09f2c8313
Mansi112 <rajawatmansi042@gmail.com>
2025-03-22 00:13:04 +0530

    Average

---
+++ Average.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class Average {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5};
+        int sum = 0;
+        for(int i = 0; i < numbers.length; i++){
+            sum += numbers[i];
+        }
+        int avg = sum / numbers.length;
+        System.out.println(avg);
+    }
+}
