d627d0bf01a502bc713a14e49128f762127ab378
Mansi112 <rajawatmansi042@gmail.com>
2025-04-11 23:50:16 +0530

    Print the Array

---
+++ PrintArray.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class PrintArray {
+    public static void main(String[] args) {
+        int[] numbers = {2,4,5,7,8,2,4,5};
+        for(int i =0; i < numbers.length; i++){
+        }
+        System.out.println(Arrays.toString(numbers));
+    }
+}
